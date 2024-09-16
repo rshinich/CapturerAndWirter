80970c16734b767c1fae400e807635b3c7efc3a0
zhang zhongrui <zhongrui@trackview.co>
2024-09-16 16:44:03 +0800

    init

---
+++ CapturerAndWirter/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CapturerAndWirter
+//
+//  Created by 张忠瑞 on 2024/9/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
